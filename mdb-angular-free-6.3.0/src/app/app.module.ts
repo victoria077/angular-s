f48564bfddda68ef99a59607b8a0a24cebe3ae7a
scary <imbamrgrey@gmail.com>
2018-11-23 14:15:54 +0100

    first commit

---
+++ mdb-angular-free-6.3.0/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
+import { ContentComponent } from './content/content.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    ContentComponent,
+    CarouselComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
+})
+export class AppModule { }
